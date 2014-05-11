50c0e9c667cbee8ff68552381a8d90a53cf65521
Adam Shinn <adam.b.shinn@gmail.com>
2014-05-10 18:02:32 -0700

    add location to printed forecast

--- weather.py
+++ weather.py
@@ -6,8 +6,9 @@ import re
 import urllib
 
 class Weather:
-    """Weather class"""
+    """weather.gov html scrubber"""
 
+    _locationpat = re.compile(r'<p class="current-conditions-location">([\w() ]+)</p>') 
     _temppattern = re.compile(r'<p class="myforecast-current-lrg">(\d+&deg;F)</p>' \
        r'<p><span class="myforecast-current-sm">(\d+&deg;C)</span></p>')
     _forecastpat = re.compile(r'<div class="one-ninth-first">[\t\n\r ]+' \
@@ -17,27 +18,30 @@ class Weather:
        r'<p>[\w</>]+</p>[\t\n\r ]+' \
        r'<p class="point-forecast-icons-\w+">(\w+: \d+) &deg;F</p>[\t\n\r ]+</div>', re.I)
 
-    def __init__(self, zipcode = 94530):
+    def __init__(self, zipcode = 94110):
         self.zipcode = zipcode
         url = "http://forecast.weather.gov/zipcity.php?inputstring=" + str(self.zipcode)
         try:
             webpage = urllib.urlopen(url)
         except IOError: 
             sys.stderr.write("Couldn't connect to {}\n".format(url))
-            return # exit countIMG without leaving current session
+            return 
         except ValueError as ValueMsg:
             sys.stderr.write("Value Error: {}\n".format(ValueMsg))
-            return # exit countIMG without leaving current session
+            return
         contents = str(webpage.read())
         webpage.close()
         temp = self.__class__._temppattern.findall(contents)
         fore = self.__class__._forecastpat.findall(contents)
+        loc  = self.__class__._locationpat.findall(contents)[0]
     
         self.temps = [re.sub(r"&deg;", r"\u00b0", tstr).decode("unicode-escape") for tstr in temp[0]]
         self.forecast = fore
+        self.loc = loc
 
     def __unicode__(self):
-        forecast_str = u"Current Temperature: {}, {}\n".format(self.temps[0], self.temps[1])
+        forecast_str = u"{self.zipcode}: {self.loc}\n".format(self=self)
+        forecast_str += u"Current Temperature: {}, {}\n".format(self.temps[0], self.temps[1])
         for forecast in self.forecast:
             forelist = list(forecast)
             if "" in forelist:
@@ -57,6 +61,6 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         zipcode = int(sys.argv[1])
     else:
-        zipcode = 94530
+        zipcode = 94110
 
     print(Weather(zipcode))
